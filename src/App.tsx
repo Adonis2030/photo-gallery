774b92585e3e533b1703ad2c63cd72c9288fd472
Adonis2030 <service.provider2030@gmail.com>
2023-11-12 09:33:04 +0200

    updated navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Navbar';
+import ListPage from './Pages/List';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+      <div className='header'>
+        <Navbar />
+      </div>
+      <ListPage />
     </div>
   );
 }
